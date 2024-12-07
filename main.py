20aae44f0b1b9503b1146fa5b972e5345722e8b3
isaacstriker <strikerisaac480@gmail.com>
2024-12-06 19:41:44 -0500

    Added word and character counters

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_num_words(text)
+    char_num = get_char_num(text)
+    print(f"{num_words} words found in the document")
+    print(char_num)
 
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_char_num(text):
+    frequency = {}
+    for char in text.lower():
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    return frequency
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
 
 main()
